54ff50277af4b1964ee9eb83890d0adf97e8634e
Rafiqul Alam Chisty <rafiqul.chisty@bkash.com>
2021-07-08 01:28:42 +0600

    added property files reading

---
+++ src/test/java/utils/getProperties.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class getProperties {
+    static Properties props = new Properties();
+
+    public static Properties getProps(){
+
+        FileReader reader = null;
+        try {
+            reader = new FileReader("system.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+// loading properties from file.
+        try {
+            props.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return props;
+    }
+
+    public static String getSpecificProperty(String proName){
+        getProps();
+        return props.getProperty(proName);
+    }
+
+    public static void main(){
+        System.out.println(getSpecificProperty("users"));
+    }
+}
